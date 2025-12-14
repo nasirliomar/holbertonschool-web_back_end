317dc79e218cecf4448e71c74d302f74c51fa95a
nasirliomar <nesirliomer019@gmail.com>
2025-12-14 14:52:26 +0000

    l

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
